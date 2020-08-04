29185fc34f01666cbf5011a32701f6fc9d204768
HrachCode <virap1000@mail.ru>
2020-08-04 19:39:45 +0400

    das-6 Hendlbars

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 var handlebars  = require('express-handlebars');
 const app = express();
- 
+
+
+
 const hbss = handlebars.create({
     layoutsDir: __dirname + '/views/layouts',
     extname: 'hbs',
@@ -10,13 +12,16 @@ const hbss = handlebars.create({
   
     
   })
+ 
   app.use(express.json())
   app.use(express.urlencoded({extended:false}))
+  app.use(express.static(__dirname + "/public"));
   app.set('view engine', 'hbs');
   app.engine('hbs',hbss.engine )
 
-app.use('/register',(req,res)=>{
-
+app.use('/register/:id',(req,res)=>{
+        console.log(req.params);
+        
     res.render('home.hbs', {
         title: "home.hbs",
         name: req.body.userName,
@@ -25,20 +30,13 @@ app.use('/register',(req,res)=>{
     
 }) 
 app.use("/contact", function(request, response){
-     
-    response.render("index.hbs", {
-        title: "Мои контакты",
-        email: "gavgav@mycorp.com",
-        phone: "+1234567890"
-    });
+     const { name,password } = request.query;
+    response.render("home.hbs")
 });
+
 app.use("/", function(request, response){
-     
-    response.render('home.hbs', {
-        title: "home.hbs",
-        name: "Hrach",
-        userAge: ""
-    });
+
+    response.render('index.hbs')
 });
 app.listen(3030,console.log(3030)
 );
