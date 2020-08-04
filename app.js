9dd8a449755b421f580ff234b4f30118334add85
HrachCode <virap1000@mail.ru>
2020-08-04 20:52:05 +0400

    das-7 mongodb

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 var handlebars  = require('express-handlebars');
 const app = express();
-
-
+const mongoose = require('mongoose');
+const User = require('./models/usermodel')
 
 const hbss = handlebars.create({
     layoutsDir: __dirname + '/views/layouts',
@@ -12,7 +12,16 @@ const hbss = handlebars.create({
   
     
   })
- 
+  const mongoURI = 'mongodb+srv://hrach:hrach@cluster0.impfb.mongodb.net/mydb?retryWrites=true&w=majority'
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(data=>{
+        console.log('mongoDb is connected');
+  }).catch(err=>{
+      console.log(err);
+  })
+
   app.use(express.json())
   app.use(express.urlencoded({extended:false}))
   app.use(express.static(__dirname + "/public"));
@@ -31,11 +40,31 @@ app.use('/register/:id',(req,res)=>{
 }) 
 app.use("/contact", function(request, response){
      const { name,password } = request.query;
-    response.render("home.hbs")
+     console.log(name,password);
+    response.render("home.hbs",{
+        name,
+        password,
+    })
+
+    const user = new User({
+           name,
+           password
+    })
+
+    user.save(function(err){
+        if(err) return console.log(err)
+            console.log('data is saved');
+    })
 });
 
-app.use("/", function(request, response){
 
+
+app.use("/", function(request, response){
+     User.find({}).then(data=>{
+         console.log(data);
+     }).catch(err=>{
+         console.log(err);
+     })
     response.render('index.hbs')
 });
 app.listen(3030,console.log(3030)
---
+++ models/usermodel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+ 
+const user = new Schema({
+  
+  name: String,
+  password: String
+});
+
+module.exports = User = mongoose.model('user',user)
